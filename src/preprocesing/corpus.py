ee7303a7f8f415f87dac48f098fb7e1433eb18a3
RozumDoKapsy <sulda.str@gmail.com>
2025-05-25 09:26:05 +0200

    feat: added corpus preprocessing (CorpusCleaner, CorpusLemmatizer)

--- src/preprocesing/corpus.py
+++ src/preprocesing/corpus.py
@@ -1,7 +1,30 @@
 from pathlib import Path
+
 import pandas as pd
+import numpy as np
+
+from sklearn.feature_extraction.text import CountVectorizer
+from corpy.udpipe import Model as UDPipeModel
+
+import re
+import string
+
+from tqdm import tqdm
 
 from typing import List, Union, Optional
+
+
+def load_stopwords(file_path: Union[Path, str], custom_stopwords: Optional[List[str]] = None) -> List[str]:
+    file_path = Path(file_path)
+    if file_path.exists():
+        stopwords_df = pd.read_json(file_path)
+        stopwords = stopwords_df[stopwords_df.columns[0]].tolist()
+    else:
+        raise FileNotFoundError(f'Stopwords file not found at location: from {file_path}')
+
+    if custom_stopwords:
+        stopwords.extend(custom_stopwords)
+    return stopwords
 
 
 class CorpusBuilder:
@@ -54,3 +77,62 @@ class CorpusBuilder:
             self.corpus.to_excel(file_path, index=False)
         else:
             raise ValueError(f'Unsupported file type: {file_path.suffix}.')
+
+
+class CorpusCleaner:
+    def __init__(self):
+        pass
+
+    def clean(self, corpus: List[str]):
+        return [self._document_clean(doc) for doc in corpus]
+
+    @staticmethod
+    def _document_clean(document: str) -> str:
+        document = document.lower()
+        document = re.sub(r'\d+', '', document)
+        document = re.sub(r'[%s]' % re.escape(string.punctuation), ' ', document)
+        document = re.sub(r'\s+', ' ', document)
+        return document
+
+
+class CorpusCzechLemmatizer:
+    def __init__(self, model: UDPipeModel):
+        self.model = model
+        self.features = np.array([], dtype=str)
+        self.features_lemmatized_dict = {}
+
+    def lemmatize(self, corpus: List[str], stopwords: List[str]):
+        self._get_corpus_features(corpus, stopwords)
+        self._feature_lemmatize()
+        return [self._document_lemmatize(doc) for doc in corpus]
+
+    def _get_corpus_features(self, corpus: List[str], stopwords: List[str]):
+        cv = CountVectorizer(min_df=1, stop_words=stopwords)
+        cv.fit_transform(corpus)
+        self.features = cv.get_feature_names_out()
+        return self.features
+
+    def _text_lemmatize(self, text: str) -> str:
+        sentence = []
+        for s in self.model.process(text):
+            for w in s.words:
+                if '<root>' not in w.lemma:
+                    sentence.append(w.lemma)
+        return ' '.join(sentence)
+
+    def _feature_lemmatize(self):
+        features_lemmatized = []
+        for f in tqdm(self.features):
+            features_lemmatized.append(self._text_lemmatize(f))
+        self.features_lemmatized_dict = dict(zip(self.features, features_lemmatized))
+        return self.features_lemmatized_dict
+
+    def _document_lemmatize(self, document: str) -> str:
+        tokens = document.split(' ')
+        tokens_lemmatized = [
+            lemma
+            for token in tokens
+            if self.features_lemmatized_dict.get(token)
+            for lemma in self.features_lemmatized_dict[token].split(' ')
+        ]
+        return ' '.join(tokens_lemmatized)
